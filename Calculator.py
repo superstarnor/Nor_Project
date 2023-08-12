14d9d09ec05273f298bffd3573fe9788835dc925
superstarnor <abdullahinor90@gmail.com>
2023-08-12 10:20:41 -0500

    Simiplied the functions

--- Calculator.py
+++ Calculator.py
@@ -1,31 +1,31 @@
 def simple_calculator():
-    print("Welcome to Simple calc") 
-    # contin = False
-    total = 0
-    to_continue = input("would you like to start, press c to conitnue or q to quit\n")
-    while(to_continue != "q"):
-        # total = 0
+    operations = {
+                "+" : lambda a,b: a + b,
+                "-" : lambda a,b: a - b,
+                "*" : lambda a,b: a * b,
+                "/" : lambda a,b: a / b if b != 0 else None }
+
+    print("Welcome to Simple calc")
+    
+    to_continue = input("Would you like to start? Press 'c' to continue or 'q' to quit\n")
+
+    while to_continue != "q":
         if to_continue == "c":
-            first_num = int(input("choose a number: "))
-            # op = {1:"+", 2:"-", 3:"*",4:"/"}
-            user_input = int(input("Select an operation, \n 1 : + \n 2: - \n 3 : *\n 4 : \ \n"))
-            sec_num = int(input("choose a second number \n"))
-            if user_input == 1:
-                total = first_num + sec_num
-                print(total)
-            elif user_input == 2: 
-                total = first_num - sec_num
-                print("Your current total: ", total)
-            elif user_input == 3: 
-                total = first_num * sec_num
-                print(total)
-            elif user_input == 4: 
-                total = first_num / sec_num
-                print(total)
-            # print(total)
-        else: 
+            first_num = float(input("Choose a number: "))
+            user_input = input("Select an operation, +, -, *, or /\n")
+            sec_num = float(input("Choose a second number:\n"))
+            if user_input in operations:
+                total = operations[user_input](first_num, sec_num)  
+                if total is not None:
+                    print("Total:", total)
+        
+            else: 
+                print("Invalid operation")
+        else:
             print("Invalid choice")
-        to_continue = input("Would you like to continue, press c to conitnue or q to quit \n") 
-    print("Final Total:", total)
-    print("Thank you bye!")
+
+        to_continue = input("Would you like to continue? Press 'c' to continue or 'q' to quit\n")
+
+    print("Thank you, goodbye!")
+
 simple_calculator()
